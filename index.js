a46b1f02e1ba7e0ccca0b4fd7a9d030b44c3c1f7
Solomon <odebisiidowu09@gmail.com>
2022-05-09 18:52:15 +0100

    completed challenge

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const upload = multer({ dest: "./uploads/" });
 
 var app = express();
 
+app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  if (req.file.size) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } else {
+    res.json({error:'Invalid Input'})
+  }
+  
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
